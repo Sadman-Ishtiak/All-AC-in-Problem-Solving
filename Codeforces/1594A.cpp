b063f1889f901354a4d247d3c19293183df807c7
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 19:09:37 +0600

    A - Consecutive Sum Riddle

---
+++ Codeforces/1594A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long int n,l,r;
+        cin >> n;
+        l = (-1)*n+1;
+        r = n;
+        cout << l << " " << r << endl;
+    }
+    return 0;
+}
