43123f32d9552ceaef180a62a2b23bfaaf8e5398
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:40:06 +0600

    Create POPULATION.cpp

---
+++ Codechef/POPULATION.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b,c;
+        cin >> a >> b >> c;
+        cout << a-b+c << endl;
+    }
+	return 0;
+}
