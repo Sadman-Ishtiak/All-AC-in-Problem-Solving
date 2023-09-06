b7f5c22c23ebb71aa9639530845a637ffc067bbc
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:36:36 +0600

    Create CREDSCORE.cpp

---
+++ Codechef/CREDSCORE.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    if(t >=750) cout << "YES" << endl;
+    else cout << "NO" << endl;
+	return 0;
+}
