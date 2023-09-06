2b2fec19a197b2e227ab3b5e12949e7efa1b0cd5
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:40:16 +0600

    Create RANKLISTPAGE.cpp

---
+++ Codechef/RANKLISTPAGE.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        float n;
+        cin >> n;
+        cout << ceil(n/25) << endl;
+    }
+	return 0;
+}
