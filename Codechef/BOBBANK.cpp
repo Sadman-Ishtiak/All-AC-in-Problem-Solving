f131ae2f2517af1167e37279c7aed3669f661a17
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:33:29 +0600

    Create BOBBANK.cpp

---
+++ Codechef/BOBBANK.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b,c,d;
+        cin >> a >> b >> c >> d;
+        cout << a+(b-c)*d << endl;
+    }
+	return 0;
+}
