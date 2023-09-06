dd2649031031a0fc2ee764782b9815b0296a3798
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:38:09 +0600

    Create TYRE.cpp

---
+++ Codechef/TYRE.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b;
+        cin >> a >> b;
+        cout << 2*a+4*b << endl;
+    }
+	return 0;
+}
