25fce56b484c581d19279733bad26abf617c0e95
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:33:50 +0600

    Create CBSPEED.cpp

---
+++ Codechef/CBSPEED.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin >> x >> y;
+    if(x<y)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+	return 0;
+}
