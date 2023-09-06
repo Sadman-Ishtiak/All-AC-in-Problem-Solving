2be74429e51e6324ccca03f0b102b66fbbd1d162
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 18:16:40 +0600

    A - Tom Riddle's Diary

---
+++ Codeforces/855A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    unordered_set<string>s;
+    while(t--){
+        string name;
+        cin >> name;
+        if(s.find(name) != s.end()) cout << "YES" << endl;
+        else cout << "NO" << endl;
+        s.insert(name);
+    }
+    return 0;
+}
