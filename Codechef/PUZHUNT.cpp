910248ca3cc2de94f2f0b5342584963dc907611e
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:40:13 +0600

    Create PUZHUNT.cpp

---
+++ Codechef/PUZHUNT.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    if(t>=6 && t<= 8) cout << "YES" << endl;
+    else cout << "NO"<< endl;
+	return 0;
+}
