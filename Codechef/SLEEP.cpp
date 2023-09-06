2b04e52833c75f36f21c14841dc9aa95022c7483
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:39:01 +0600

    Create SLEEP.cpp

---
+++ Codechef/SLEEP.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        if (n<7)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        cout << "NO" << endl;
+    }
+	return 0;
+}
