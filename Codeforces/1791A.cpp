cc55a166e790bff8486960c9114ebbc1c7f87b3a
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 19:13:33 +0600

    A - Codeforces Checking

---
+++ Codeforces/1791A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        char i;
+        cin >> i;
+        if(i == 'c' || i == 'o' || i == 'd' || i == 'e' || i == 'f' || i == 'r' || i == 's') cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+    return 0;
+}
