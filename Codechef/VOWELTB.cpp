9f56f703f80e307aca6b5a6d33370e7c52fcd5a1
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:38:48 +0600

    Create VOWELTB.cpp

---
+++ Codechef/VOWELTB.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+    char t;
+    cin >> t;
+    if(t == 'A' || t == 'E' || t == 'I' || t == 'O' || t == 'U') cout << "Vowel";
+    else cout << "Consonant";
+	return 0;
+}
