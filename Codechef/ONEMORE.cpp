3466d5c532438c5ce820c960c6d620708faede1d
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:39:55 +0600

    Create ONEMORE.cpp

---
+++ Codechef/ONEMORE.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int a;
+        cin >> a ;
+        if(a > 24)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+	return 0;
+}
