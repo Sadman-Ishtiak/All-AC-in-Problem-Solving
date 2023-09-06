b91815ff1b26a0b29514f998cdb4c6739611905f
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 21:49:06 +0600

    A - Game with Board

---
+++ Codeforces/1841A.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        if(n>=5)
+            cout << "Alice" << endl;
+        else 
+            cout << "Bob" << endl;
+    }
+    return 0;
+}
