dbdc056192083fdcda07f8179d5e471f5bb4c83b
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 20:54:52 +0600

    A - LCM Problem

---
+++ Codeforces/1389A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x,y,l,r;
+        cin >> l >> r;
+        if(2*l > r){
+            x = -1;
+            y = -1;
+        }
+        else{
+            x = l;
+            y = 2*l;
+        }
+        cout << x << " " << y << endl;
+    }
+    return 0;
+}
