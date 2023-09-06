9505bae50c667bb0e7c101606214b055f95a8f6b
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:37:53 +0600

    Create JENGA.cpp

---
+++ Codechef/JENGA.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b;
+        cin >> a >> b;
+        if(b%a == 0) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+	return 0;
+}
