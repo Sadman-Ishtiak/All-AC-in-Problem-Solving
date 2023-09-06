12ee58fcdb6aebafe195199cf845d9b621bd443d
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:40:12 +0600

    Create PROINC.cpp

---
+++ Codechef/PROINC.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b;
+        cin >> a >> b;
+        cout << 1.1*a -(a-b) << endl;
+    }
+	return 0;
+}
