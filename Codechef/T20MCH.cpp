03c1b7382879bcf7829737d98bcd3202fafecd84
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:38:28 +0600

    Create T20MCH.cpp

---
+++ Codechef/T20MCH.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x,y,z;
+    cin >> x >> y >> z;
+    if((20-y)*36+z > x) cout << "YES" << endl;
+    else cout << "NO" << endl;
+	return 0;
+}
