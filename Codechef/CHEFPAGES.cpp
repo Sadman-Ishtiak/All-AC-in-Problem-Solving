5ad977bce665bf5c2b2eb4f8d203b981b2040279
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:34:01 +0600

    Create CHEFPAGES.cpp

---
+++ Codechef/CHEFPAGES.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    if(a == 0) cout << "https://www.codechef.com/practice" << endl;
+    if(a == 1 && b == 0) cout << "https://www.codechef.com/contests" << endl;
+    if(a == 1 && b == 1) cout << "https://discuss.codechef.com" << endl;
+	return 0;
+}
