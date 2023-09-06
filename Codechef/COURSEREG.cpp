0a0b4ce94d331d67d0f2a1afc63267d079d8f49b
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:34:16 +0600

    Create COURSEREG.cpp

---
+++ Codechef/COURSEREG.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b,c;
+        cin >> a >> b >> c;
+        if (a+c <= b)
+        {
+            cout << "Yes" << endl;
+        }
+        else cout << "No" << endl;
+    }
+	return 0;
+}
