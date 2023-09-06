8ab977ddd56a73db0a860a0b774fca4f6bbbd1a3
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:37:05 +0600

    Create EXPIRY.cpp

---
+++ Codechef/EXPIRY.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b,c;
+        cin >> a >> b >> c;
+        if(b*c>= a) cout << "Yes" << endl;
+        else cout << "No" << endl;
+    }
+	return 0;
+}
