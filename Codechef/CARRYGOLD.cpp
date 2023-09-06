548734a98ac214a7685133ea7c439f4a5060b81e
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:33:49 +0600

    Create CARRYGOLD.cpp

---
+++ Codechef/CARRYGOLD.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n,x,y;
+        cin >> n >> x >> y;
+        n++;
+        if(n*y >= x) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+	return 0;
+}
