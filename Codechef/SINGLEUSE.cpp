750e8dac0087d39782a2b984d734904cded80b07
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:39:03 +0600

    Create SINGLEUSE.cpp

---
+++ Codechef/SINGLEUSE.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        float a,b,c;
+        cin >> a >> b >> c;
+        cout << ceil((a-c)/b+1) << endl;
+    }
+	return 0;
+}
