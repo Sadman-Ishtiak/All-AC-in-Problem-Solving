34fce272b8d62e9a936234bbb4a12cf4f404d2e9
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:40:05 +0600

    Create POLYBAGS.cpp

---
+++ Codechef/POLYBAGS.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        float n;
+        cin >> n;
+        cout << ceil(n/10) << endl;
+    }
+	return 0;
+}
