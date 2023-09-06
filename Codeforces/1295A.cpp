23c27a9a6ad4758772387a8bea26bcac25db3ef1
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 18:11:05 +0600

    A - Display The Number

---
+++ Codeforces/1295A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        if(n & 1 == 1){
+            cout << 7 ;
+            n -= 3;
+        }
+        while(n > 0){
+            cout << 1;
+            n -= 2;
+        }
+        cout << endl;
+    }
+    return 0;
+}
