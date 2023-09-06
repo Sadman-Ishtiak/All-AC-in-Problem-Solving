7164939708797a0d16da51dbf8a1e51587ffa4d2
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:39:55 +0600

    Create ONEFULPAIRS.cpp

---
+++ Codechef/ONEFULPAIRS.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    if (a+b+a*b == 111)
+    {
+        cout << "Yes" << endl;
+    }
+    else cout << "No" << endl;
+	return 0;
+}
