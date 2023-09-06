49591c92b162d9d678c64f5e2ed2ed6601ba60f5
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:38:34 +0600

    Create SUBSCRIBE_.cpp

---
+++ Codechef/SUBSCRIBE_.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        float a,b;
+        cin >> a >> b;
+        cout << ceil(a/6)*b <<  endl;
+    }
+	return 0;
+}
