f2a00fa44767956aa3ac5d876cffebdee3b0a83c
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:38:08 +0600

    Create TYRES.cpp

---
+++ Codechef/TYRES.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        if(n%4 >= 2) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+	return 0;
+}
