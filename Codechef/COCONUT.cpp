029218008cc11c8e3d9318430fa96dc38063e8ce
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:34:11 +0600

    Create COCONUT.cpp

---
+++ Codechef/COCONUT.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b,x,y;
+        cin >> a >> b >> x >> y;
+        cout << x/a + y/b << endl;
+    }
+	return 0;
+}
