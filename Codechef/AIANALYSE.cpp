b45b2eb562dba92f3d24bbeb122182bdacce9ffa
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:33:00 +0600

    Create AIANALYSE.cpp

---
+++ Codechef/AIANALYSE.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    if (t<=1000)
+    {
+        cout << "Yes" ;
+    }
+    else cout << "No";    
+	return 0;
+}
