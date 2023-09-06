ab9e25f3385737d871b88fa6df59bf3c400d24f1
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:34:03 +0600

    Create CHEFSCORE.cpp

---
+++ Codechef/CHEFSCORE.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b,c;
+        cin >> a >> b >> c;
+        if(c/b <= a && c%b == 0) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+	return 0;
+}
