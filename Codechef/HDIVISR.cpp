7586c18fee906ba510a754603774246238a067de
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:37:38 +0600

    Create HDIVISR.cpp

---
+++ Codechef/HDIVISR.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    for (int i = 10; i > 0; i--){
+        if(t%i == 0){cout << i; break;}
+    }
+	return 0;
+}
