5ee170babd35228cd738885a3254fb3f440087fc
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:38:54 +0600

    Create SOCKS1.cpp

---
+++ Codechef/SOCKS1.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    if(a == b || b == c || a == c) cout << "YES" << endl;
+    else cout << "NO" << endl;
+	return 0;
+}
