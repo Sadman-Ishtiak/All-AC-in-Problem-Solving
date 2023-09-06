bd0500a3d8a7b6f23a6af0bd5995e19afb29899a
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-06 13:39:31 +0600

    Create MARKSTW.cpp

---
+++ Codechef/MARKSTW.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    if (2*b <= a)
+    {
+        cout << "Yes" ;
+    }
+    else cout << "No";
+	return 0;
+}
