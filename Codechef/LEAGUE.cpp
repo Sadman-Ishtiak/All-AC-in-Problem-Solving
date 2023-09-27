6ff7329e98dcfd474dee14ffde7943a2ab5e3b2e
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-09-27 19:18:34 +0600

    Create LEAGUE.cpp

---
+++ Codechef/LEAGUE.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+
+void solve(){
+    int n;
+    cin >> n;
+    cout << (n/2)*3 << endl;
+}
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+	return 0;
+}
