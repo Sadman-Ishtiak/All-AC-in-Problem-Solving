caff3135174290abc88e8f9819c0244c6fa25090
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-10-04 18:37:39 +0600

    Create NEWSPAPER.cpp

---
+++ Codechef/NEWSPAPER.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void solve(){
+    
+}
+int main() {
+    int t;
+    cin >> t;
+    if(t >= 8) cout << "YES" << endl;
+    else cout << "NO" << endl;
+	return 0;
+}
