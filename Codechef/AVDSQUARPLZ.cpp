8b91a74182833f90fbfc81bea68f59d4a7f347bd
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-10-11 23:49:00 +0600

    Create AVDSQUARPLZ.cpp

---
+++ Codechef/AVDSQUARPLZ.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cout << i+1 << " ";
+    }
+    cout << endl;
+}
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+	return 0;
+}
