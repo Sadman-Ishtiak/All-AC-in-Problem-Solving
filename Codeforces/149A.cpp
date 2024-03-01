588f2d274acaf632428e5ec710831fea57281afc
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-03-01 19:48:39 +0600

    Create 149A.cpp

---
+++ Codeforces/149A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+void solve(){
+    int n;
+    cin >> n;
+    vector<int>a(12);
+    int ans = 0;
+    for (int i = 0; i < 12; i++) cin >> a[i];
+    sort(a)
+    reverse(all(a));
+    while (n>0 && ans<12){
+        n-=a[ans];
+        ans++;
+    }
+    if(ans == 12 && n > 0) ans = -1;
+    cout << ans;
+}
+int32_t main() {
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+	return 0;
+}
