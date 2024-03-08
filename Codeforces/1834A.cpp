685170e5526408351de9a94bf2c08b4caf4eeee2
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-03-08 18:18:19 +0600

    Create 1834A.cpp

---
+++ Codeforces/1834A.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+void solve(){
+    int n;
+    cin >> n;
+    vector<int>a(n);
+    int ans = 0, pos = 0;
+    for (int i = 0; i < n; i++){
+        cin >> a[i];
+        if(a[i] == 1) pos++;
+    }
+    int neg = n-pos;
+    if(pos >= n){
+        if(neg %2 == 1 ) ans = 1;
+    } else{
+        while ((pos-neg) < 0 || neg%2 == 1){
+            ans++;
+            neg--;
+            pos++;
+        }
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+	return 0;
+}
