05b9195b6f8ee73143573419f6db3a2a30114c86
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-03-08 18:18:34 +0600

    Create 1901A.cpp

---
+++ Codeforces/1901A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+void solve(){
+    int n,x;
+    cin >> n >> x;
+    vector<int>a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    int ans = a[0];
+    for (int i = 1; i < n; i++){
+        ans = max(ans, a[i]-a[i-1]);
+    }
+    ans = max(ans, 2*(x-a[n-1]));
+    cout << ans << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+	return 0;
+}
