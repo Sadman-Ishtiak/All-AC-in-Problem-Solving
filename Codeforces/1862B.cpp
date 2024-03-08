105d97df4c4f14908392ffe45e80496ef1d87a9a
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-03-08 18:18:24 +0600

    Update 1862B.cpp

--- Codeforces/1862B.cpp
+++ Codeforces/1862B.cpp
@@ -1,27 +1,37 @@
 #include <bits/stdc++.h>
-#define ll long long
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
 using namespace std;
-
-int main() {
-    int t;
-    cin >> t;
-    while(t--){
-        char s1[5001] = {'\0'}, s2[5001] = {'\0'};
-        scanf("%s",s1);
-        scanf("%s",s2);
-        bool flag = false;
-        // Ptocessing
-        vector<int>lz1,lz2,fo1,fo2;
-        int i = 0;
-        while(s1[i] != '\0'){
-            if(s1[i] == '0' && s2[i] == '0' && s1[i+1] == '1' && s2[i+1] == '1') flag = true;
-            i++;
-        }
-        if(s1[1] == '1' && s2[1] == '1') flag = true;
-        if(s1[i-2] == '0' && s2[i-2] == '0') flag = true;
-        // Printing answer
-        if(flag) cout << "YES" << endl;
-        else cout << "NO" << endl;
+void solve(){
+    int n;
+    cin >> n;
+    vector<int>a(n);
+    for (int i = 0; i < n; i++){
+        cin >> a[i];
     }
+    vector<int>ans;
+    ans.push_back(a[0]);
+    for (int i = 1; i < n; i++){
+        if(a[i-1] > a[i]){
+            ans.push_back(1);
+            ans.push_back(a[i]);
+        } else ans.push_back(a[i]);
+    }
+    cout << ans.size() << endl;
+    for(auto i : ans) cout << i << " ";
+    cout << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
 	return 0;
 }
