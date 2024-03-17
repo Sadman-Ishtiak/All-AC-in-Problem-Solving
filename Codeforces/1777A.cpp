ab48af99f880309a885d5e14ab6ca6fc1606e9fa
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-03-17 08:46:28 +0600

    Create 1777A.cpp

---
+++ Codeforces/1777A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+void solve(){
+    int n, ans = 0;
+    cin >> n;
+    vector<int>a(n);
+    cin >> a[0];
+    for (int i = 1; i < n; i++){
+        cin >> a[i];
+        if(a[i] %2 == a[i-1]%2) ans++;
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+	return 0;
+}
