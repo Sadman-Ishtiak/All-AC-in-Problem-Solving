4d25e5d0b6e80fe63deb8c79695037008d95fb77
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-03-17 08:46:19 +0600

    Create 1669B.cpp

---
+++ Codeforces/1669B.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+void solve(){
+    int n;
+    cin >> n;
+    vector<int>a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    sort(a)
+    int ans = -1;
+    int k = a[0], count = 0;
+    for (int i = 0; i < n; i++){
+        if(k == a[i]) count++;
+        else{
+            k = a[i];
+            count = 1;
+        }
+        if(count >= 3){
+            ans = k;
+            // break;
+        }
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+	return 0;
+}
