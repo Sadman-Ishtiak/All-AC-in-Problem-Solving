8b3acfcd566f193d81607712cc527d6a784eb637
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-03-17 08:46:22 +0600

    Create 1742B.cpp

---
+++ Codeforces/1742B.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+void solve(){
+    int n;
+    cin >> n;
+    vector<int>a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a)
+    for (int i = 1; i < n; i++){
+        if(a[i-1] >= a[i]){
+            NO;
+            return;
+        }
+    }
+    YES;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+	return 0;
+}
