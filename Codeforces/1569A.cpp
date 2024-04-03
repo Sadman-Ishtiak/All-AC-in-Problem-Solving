108937201fe17c57d0dd488087c2dc354b9a3af8
Sadman Ishtiak <sadmanishtiak1@gmail.com>
2024-04-03 13:24:54 +0600

    Create 1569A.cpp

---
+++ Codeforces/1569A.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+void solve(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int ans = 0;
+    for (int j = 0; j < n; j++){
+        ans = 0;
+        int i;
+        for (i = j; i < n; i++){
+            if(s[i] == 'a') ans++;
+            else ans--;
+            
+            if(ans == 0){
+                cout << j+1 << " " << i+1 << endl;
+                return;
+            }
+        }
+    }
+    cout << "-1 -1" << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+	return 0;
+}
