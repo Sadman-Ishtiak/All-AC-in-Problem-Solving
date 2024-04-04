deefa2cee0adf67979f15fcc73a4b45bd9421d88
Sadman Ishtiak <sadmanishtiak1@gmail.com>
2024-04-04 13:08:31 +0600

    Create 1851A.cpp

---
+++ Codeforces/1851A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+void solve(){
+    int n,m,h,k;
+    cin >> n >> m >> k >> h;
+    vector<int>heights(n);
+    int ans = 0;
+    for (int i = 0; i < n; i++){
+        cin >> heights[i];
+        if (h != heights[i] && (h-heights[i])%k == 0 && abs(h-heights[i])/k < m)
+            ans++;
+    }
+     cout << ans << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+	return 0;
+}
