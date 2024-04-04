1c23fe8c7190830dc01e1fb1461a49b371dc93bc
Sadman Ishtiak <sadmanishtiak1@gmail.com>
2024-04-04 13:08:30 +0600

    Create 1676C.cpp

---
+++ Codeforces/1676C.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+void solve(){
+    int n, m;
+    cin >> n >> m;
+    string words[n];
+    for (int i = 0; i < n; i++) cin >> words[i];
+    int ans = INT64_MAX;
+    for (int i = 0; i < n; i++){
+        for (int j = i+1; j < n; j++){
+            int temp = 0;
+            for (int k = 0; k < m; k++){
+                temp += abs(words[i][k] - words[j][k]);
+            }
+            ans = min(ans, temp);
+        }
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+	return 0;
+}
