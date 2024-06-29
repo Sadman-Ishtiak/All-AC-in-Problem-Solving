25efb9ddd113d27b9bc81311fcd3146760745563
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-06-30 00:00:02 +0600

    Create abc293_b.cpp

---
+++ Atcoder/abc293_b.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) cin >> a[i];
+    int ans = 0;
+    int an[n] = {0};
+    for (int i = 0; i < n; i++){
+        if(an[i] == 0) an[a[i] - 1]++;
+    }
+    for (int i = 0; i < n; i++){
+        if(an[i] == 0) ans++;
+    }
+    cout << ans << endl;
+    for (int i = 0; i < n; i++){
+        if(an[i] == 0) cout << i+1 << " ";
+    }
+}
+int32_t main() {
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+    return 0;
+}
