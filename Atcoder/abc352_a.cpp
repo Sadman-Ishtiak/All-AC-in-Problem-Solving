517b5f387c81e507b63b3caf95d1562bb09e5f8a
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-06-29 21:34:01 +0600

    Create abc352_a.cpp

---
+++ Atcoder/abc352_a.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "Yes" << '\n'
+#define     NO              cout << "No" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n, x, y, z;
+    cin >> n >> x >> y >> z;
+    if (x <= z && z <= y) YES;
+    else if (y <= z && z <= x) YES;
+    else NO;
+}
+int32_t main() {
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+    return 0;
+}
