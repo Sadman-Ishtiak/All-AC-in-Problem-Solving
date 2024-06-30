07ad361201bd8ba42ab8a165c2c21a4d3ee804a2
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-06-30 16:18:07 +0600

    Create abc354_a.cpp

---
+++ Atcoder/abc354_a.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    int ans = 0, tree = 0;
+    for (int i = 0; i < 64; i++){
+        ans++;
+        tree += pow(2,i);
+        // cout << ans << " " << tree << endl;
+        if(tree > n){
+            cout << ans;
+            return;
+        }
+    }
+    
+}
+int32_t main() {
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+    return 0;
+}
