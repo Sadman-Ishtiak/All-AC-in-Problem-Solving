9bf6843f8f2de7dc884487267454a91755bbfd10
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-06-30 15:00:38 +0600

    Create abc101_a.cpp

---
+++ Atcoder/abc101_a.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    string s;
+    cin >> s;
+    int ans = 0;
+    for (int i = 0; i < 4; i++){
+        if(s[i] == '+') ans++;
+        else ans--;
+    }
+    cout << ans;
+}
+int32_t main() {
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+    return 0;
+}
