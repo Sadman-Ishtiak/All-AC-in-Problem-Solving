6d14b0b987f4c53a6a645dbb2786e19596f71c76
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-07-10 13:07:50 +0600

    Update 1699A.cpp

--- Codeforces/1699A.cpp
+++ Codeforces/1699A.cpp
@@ -1,36 +1,25 @@
-#include <bits/stdc++.h>
-#define ll long long
-#define ld long double
-#define all(x) x.begin(),x.end()
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
 using namespace std;
-vector<int> SieveOfEratosthenes(int n);
 
 void solve(){
     int n;
     cin >> n;
-    if(n >= 1900) cout << "Division 1" << endl;
-    else if (n >= 1600) cout << "Division 2" << endl;
-    else if (n >= 1400) cout << "Division 3" << endl;
-    else cout << "Division 4" << endl;
+    if(n%2 == 0) cout << n/2 << " 0 " << n/2 << endl;
+    else cout << -1 << endl;
 }
-int main() {
+int32_t main() {
     int t = 1;
     cin >> t;
     while(t--) solve();
-	return 0;
-}
-vector<int> SieveOfEratosthenes(int n){
-    bool prime[n + 1];
-    vector<int>a;
-    memset(prime, true, sizeof(prime));
-    for (int p = 2; p * p <= n; p++) {
-        if (prime[p] == true) {
-            for (int i = p * p; i <= n; i += p)
-                prime[i] = false;
-        }
-    }
-    for (int p = 2; p <= n; p++)
-        if (prime[p])
-            a.push_back(p);
-    return a;
+    return 0;
 }
