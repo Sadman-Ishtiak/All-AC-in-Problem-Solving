bea96bb1faab43b980d0d9cf35fc9f2fd53acb48
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-07-11 17:40:49 +0600

    Create 1615A.cpp

---
+++ Codeforces/1615A.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int>a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    sort(a)
+    int ans = 0, sum = 0;
+    for (int i = 0; i < n; i++){
+        sum += a[i];
+    }
+    if(sum%n == 0) cout << 0 << endl;
+    else cout << 1 << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
