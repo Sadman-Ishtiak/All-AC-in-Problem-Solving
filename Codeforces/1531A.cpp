b6e464e46200f5f0378a2881a0d5073dbc3ff0ca
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-07-16 23:21:14 +0600

    Create 1531A.cpp

---
+++ Codeforces/1531A.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    string ans = "blue";
+    string s;
+    bool change = true;
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+        if(s == "lock") change = false;
+        else if(s == "unlock") change = true;
+        else {
+            if(change)
+                ans = s;
+        }
+    }
+    cout << ans;
+}
+int32_t main() {
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+    return 0;
+}
