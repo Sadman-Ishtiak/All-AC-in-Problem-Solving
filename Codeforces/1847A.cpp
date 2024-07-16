c52f9a2f02ed9844345d58eea83e4ef0afcf3f44
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-07-16 23:21:18 +0600

    Create 1847A.cpp

---
+++ Codeforces/1847A.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+void solve(){
+    int n, k;
+    cin >> n >> k;
+    vector<int>a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    vector<int>diffs;
+    for (int i = 1; i < n; i++) {
+        diffs.push_back(abs(a[i] - a[i-1]));
+    }
+    sort(diffs)
+    int l = diffs.size();
+    int ans = 0;
+    for (int i = 0; i < l-k+1; i++) {
+        ans += diffs[i];
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
