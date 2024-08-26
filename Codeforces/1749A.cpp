af0e6c095e4ecf0bb30c52f9ce62fc0f924beca8
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-08-26 15:00:56 +0600

    Create 1749A.cpp

---
+++ Codeforces/1749A.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n, m;
+    cin >> n >> m;
+    vector<int>row(n,0), col(n,0);
+    for (int i = 0; i < m; i++) {
+        int x, y;
+        cin >> x >> y;
+        row[x-1]++;
+        col[y-1]++;
+    }
+    if(n>m) YES;
+    else NO;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
