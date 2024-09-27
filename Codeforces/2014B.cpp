becb7513487763749e9da712f34336d266d6233f
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-09-27 09:58:51 +0600

    Create 2014B.cpp

---
+++ Codeforces/2014B.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n,k;
+    cin >> n >> k;
+    if(((n*(n+1)/2) - (n-k)*(n-k+1)/2)%2 == 0) YES;
+    else NO;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
