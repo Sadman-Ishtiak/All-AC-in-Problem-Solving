aae6dc7f9eb1de009f3d8887df2a3f322add54f8
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-09-27 09:58:50 +0600

    Create 2014A.cpp

---
+++ Codeforces/2014A.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n, k;
+    cin >> n >> k;
+    vector<int>a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int ans = 0, curr = 0;
+    for (int i = 0; i < n; i++) {
+        if(a[i] >= k) curr+= a[i];
+        if(a[i] == 0 && curr > 0) {
+            curr--;
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
