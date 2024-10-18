367f63a5233dc3ab1aac0dbf12b77ad4483e6afd
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-10-18 07:57:58 +0600

    Create 1497A.cpp

---
+++ Codeforces/1497A.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int>a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    sort(a)
+    vector<int>ans(n);
+    int pos = 1, neg = n-1;
+    ans[0] = a[0];
+    for (int i = 1; i < n; i++) {
+        if(a[i-1] == a[i]) {
+            ans[neg] = a[i];
+            neg--;
+        } else {
+            ans[pos] = a[i];
+            pos++;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
