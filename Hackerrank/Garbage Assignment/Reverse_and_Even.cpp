1e95dd65c632fe66982da432510de52535333b22
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-11-08 20:25:52 +0600

    Create Reverse_and_Even.cpp

---
+++ Hackerrank/Garbage Assignment/Reverse_and_Even.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if(i%2 == 0) cout << a[i] << " ";
+    }
+    cout << endl;
+}
+int32_t main() {
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+    return 0;
+}
