e65e1e598489a65fa2a0750bad674aa431faf8b6
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-11-08 23:44:39 +0600

    Create kikiboba.cpp

---
+++ Kattis/kikiboba.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    string s;
+    cin >> s;
+    int n = s.length();
+    int b = 0, k = 0;
+    for (int i = 0; i < n; i++) {
+        if(s[i] == 'b') b++;
+        if(s[i] == 'k') k++;
+    }
+    if(b > k) cout << "boba\n";
+    else if(k > b) cout << "kiki\n";
+    else if(b == k && b > 0)cout << "boki\n";
+    else cout << "none\n";
+}
+int32_t main() {
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+    return 0;
+}
