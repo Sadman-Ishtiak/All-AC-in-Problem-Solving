aac4e3d4ca70e06c1aa98d1f0e419f91b41ccd0c
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-11-08 23:44:42 +0600

    Create skak.cpp

---
+++ Kattis/skak.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int x1,x2,y1,y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    if(x1 == x2 && y1 == y2) cout << 0 << endl;
+    else if(x1 == x2 || y1 == y2) cout << 1 << endl;
+    else cout << 2 << endl;
+}
+int32_t main() {
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+    return 0;
+}
