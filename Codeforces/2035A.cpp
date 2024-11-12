d124ed0bbad547cc259f006f0cbf33882f71c14e
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-11-12 18:14:13 +0600

    Create 2035A.cpp

---
+++ Codeforces/2035A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n, m, r, c;
+    cin >> n >> m >> r >> c;
+    int ans = m - c + (2*m - 1)*(n - r);
+    cout << ans << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
