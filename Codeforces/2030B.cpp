000f51b4ea5d68dfb7b0a3ac5c43e65be0a2e46e
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-11-12 18:14:12 +0600

    Create 2030B.cpp

---
+++ Codeforces/2030B.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+string s = "";
+
+
+void solve(){
+    int n;
+    cin >> n;
+    cout << 1;
+    for (int i = 1; i < n; i++) {
+        cout << 0;
+    }
+    cout << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
