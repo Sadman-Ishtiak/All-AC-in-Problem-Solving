e43dd02b4810212d707f166c231bc0744e8c5134
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-11-12 21:00:52 +0600

    Create 1657B.cpp

---
+++ Codeforces/1657B.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n, b, x, y;
+    cin >> n >> b >> x >> y;
+    int ans = 0;
+    int curritem = 0;
+    int i = 1;
+    while (i <= n) {
+        if(curritem + x <= b){
+            curritem += x;
+        } else curritem -= y;
+        ans += curritem;
+        i++;
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
