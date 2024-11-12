0605b1cc628fb17586bb33a5404ceaf0b9cdfd6f
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-11-13 00:09:32 +0600

    Create 1942A.cpp

---
+++ Codeforces/1942A.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n, k;
+    cin >> n >> k;
+    vector<int>a(n);
+    if(k == 1) {
+        for (int i = 0; i < n; i++) {
+            cout << i+1 << " ";
+        }
+    } else if (k == n) {
+        for (int i = 0; i < n; i++) {
+            cout << 1 << " ";
+        }
+    } else {
+        cout << -1;
+    }
+    cout << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
