b57913f676ef210f6d7148482eee6e428673c72f
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-11-12 18:14:09 +0600

    Create 2025A.cpp

---
+++ Codeforces/2025A.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    string s1, s2;
+    cin >> s1 >> s2;
+    int ans = 0;
+    int l1 = s1.length(), l2 = s2.length();
+    int n = min(l1, l2);
+    for (int i = 0; i < n; i++) {
+        if(s1[i] == s2[i]) {
+            ans++;
+        } else {
+            break;
+        }
+    }
+    if(ans > 0) ans--;
+    cout << l1 + l2 - ans << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
