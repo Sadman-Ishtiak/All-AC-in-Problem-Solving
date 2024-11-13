66236fcdbe78fb4010de9533b9ff59e8b26863a9
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2024-11-13 20:40:01 +0600

    Create 1581A.cpp

---
+++ Codeforces/1581A.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define     MOD             1000000007
+using namespace std;
+
+
+
+void solve(){
+    int n;
+    cin >> n;
+    int n2 = 2*n;
+    int ans = 1;
+    for (int i = 3; i <= n2; i++) {
+        ans = (ans * i)%MOD;
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
