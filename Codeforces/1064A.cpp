f48e0bb0950a71cc763d9eba2542d3431edfef5e
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-02-16 21:57:18 +0600

    Create 1064A.cpp

---
+++ Codeforces/1064A.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    vector<int>a(3);
+    for (int i = 0; i < 3; i++) {
+        cin >> a[i];
+    }
+    sort(a)
+    if(a[0] + a[1] > a[2]) cout << 0 << endl;
+    else cout << a[2]+1 - a[0] - a[1] << endl;
+}
+int32_t main() {
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+    return 0;
+}
