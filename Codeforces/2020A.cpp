9d5369c5304200ab459462e1a892d57ff67dc691
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-02-16 21:57:34 +0600

    Create 2020A.cpp

---
+++ Codeforces/2020A.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n, k;
+    cin >> n >> k;
+    int ans = 0;
+    if(k == 1) {
+        cout << n << endl;
+        return;
+    }
+    while (n) {
+        ans += n%k;
+        n /= k;
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
