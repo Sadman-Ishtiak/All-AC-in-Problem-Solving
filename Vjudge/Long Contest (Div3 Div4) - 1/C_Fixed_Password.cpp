a0d31e571a51a465cd75b9e701bef0319cbed6f2
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-03-30 13:00:29 +0600

    Create C_Fixed_Password.cpp

---
+++ Vjudge/Long Contest (Div3 Div4) - 1/C_Fixed_Password.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     Yes             cout << "Yes" << '\n'
+#define     No              cout << "No" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+int x;
+
+int32_t main() {
+    int t = 1;
+    // cin >> t;
+    while(cin >> x) {
+        if(x == 1999) {
+            cout << "Correct\n";
+            return 0;
+        } else cout << "Wrong\n";
+    }
+    return 0;
+}
