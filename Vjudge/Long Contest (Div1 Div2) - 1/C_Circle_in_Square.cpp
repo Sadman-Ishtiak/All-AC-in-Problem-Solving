35bc6ccd6b67a703e61eb7b0546724a5dea5a134
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-03-30 13:00:16 +0600

    Create C_Circle_in_Square.cpp

---
+++ Vjudge/Long Contest (Div1 Div2) - 1/C_Circle_in_Square.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     Yes             cout << "Yes" << '\n'
+#define     No              cout << "No" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    ld r;
+    cin >> r;
+    ld d = 2*r;
+    ld ans = (d*d) - (acosl(-1)*r*r);
+    printf("%.2Lf\n", ans);
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    for (int i = 1; i <= t; i++) {
+        cout << "Case " << i << ": ";
+        solve();
+    }
+    return 0;
+}
