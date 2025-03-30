8e5d805eb9019daeffe5d24d8a276c246e27af52
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-03-30 13:01:00 +0600

    Create W_Sum_Digits.cpp

---
+++ Vjudge/Long Contest (Div3 Div4) - 1/W_Sum_Digits.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     Yes             cout << "Yes" << '\n'
+#define     No              cout << "No" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        ans += (int)(s[i] - '0');
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+    return 0;
+}
