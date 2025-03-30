810a810969a6db395ae6ee0c2b0c70c88c5d7d7b
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-03-30 13:00:54 +0600

    Create S_Count_Subarrays.cpp

---
+++ Vjudge/Long Contest (Div3 Div4) - 1/S_Count_Subarrays.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     Yes             cout << "Yes" << '\n'
+#define     No              cout << "No" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int>a(n+1, INT_MIN);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+    int ans = 0;
+    int ctr = 0;
+    for (int i = 0; i < n; i++) {
+        if(a[i-1] < a[i]) {
+            ctr++;
+        }
+        else {
+            ctr = 1;
+        }
+        ans += ctr;
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
