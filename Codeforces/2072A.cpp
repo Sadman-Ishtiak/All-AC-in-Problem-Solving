ff3514a5f2d6da6d0b290c43d81406ad8f9b8aaa
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-03-30 12:59:53 +0600

    Create 2072A.cpp

---
+++ Codeforces/2072A.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     Yes             cout << "Yes" << '\n'
+#define     No              cout << "No" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n, k, p;
+    cin >> n >> k >> p;
+    k = abs(k);
+    if(n*p < k) cout << -1 << endl;
+    else {
+        int ans = (k + p - 1) / p;
+        cout << ans << endl;
+    }
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
