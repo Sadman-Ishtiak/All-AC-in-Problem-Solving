f5a588904034a484e50acffd3a28833b760b2b15
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-03-30 13:00:35 +0600

    Create G_Normal_Problem.cpp

---
+++ Vjudge/Long Contest (Div3 Div4) - 1/G_Normal_Problem.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     Yes             cout << "Yes" << '\n'
+#define     No              cout << "No" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    string s;
+    cin >> s;
+    int l = s.length();
+    string ans = "";
+    for (int i = l - 1; i >= 0; i--) {
+        if(s[i] == 'p') ans += "q";
+        if(s[i] == 'q') ans += "p";
+        if(s[i] == 'w') ans += "w";
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
