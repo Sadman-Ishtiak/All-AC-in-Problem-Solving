d6f3c2ffda053c9d7c7441a9762d2893726f2cb0
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-05-08 23:38:20 +0600

    Create 746A.cpp

---
+++ Codeforces/746A.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     Yes             cout << "Yes" << '\n'
+#define     No              cout << "No" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int a, b, c;
+    cin >> a >> b >> c;
+    int ctr = min({a, b/2, c/4});
+    cout << ctr + ctr*2 + ctr*4<< endl;
+}
+int32_t main() {
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+    return 0;
+}
