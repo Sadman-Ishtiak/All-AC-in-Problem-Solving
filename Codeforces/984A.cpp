c2c659703e7c854a4a49164f3142ae830834428d
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-05-09 14:33:05 +0600

    Create 984A.cpp

---
+++ Codeforces/984A.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     Yes             cout << "Yes" << '\n'
+#define     No              cout << "No" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    vector<int>a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    sort(a)
+    cout << a[(n-1)/2] << endl;
+}
+int32_t main() {
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+    return 0;
+}
