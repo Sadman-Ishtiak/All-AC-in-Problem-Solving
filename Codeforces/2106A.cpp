d806cb3de2e495ce2b134761aa7b2be9da216a66
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-06-06 20:40:41 +0600

    Create 2106A.cpp

---
+++ Codeforces/2106A.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     Yes             cout << "Yes" << '\n'
+#define     No              cout << "No" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n, ans = 0;
+    cin >> n;
+    string s;
+    cin >> s;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if(i == j) ans += ((s[j] == '0')?1:0);
+            else ans += ((s[j] == '1')?1:0);
+        }
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
