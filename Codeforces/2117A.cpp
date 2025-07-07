99a2a87d87fbe01bfa425ec6f8deafc87302022b
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-07-07 22:48:34 +0600

    Create 2117A.cpp

---
+++ Codeforces/2117A.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     Yes             cout << "Yes" << '\n'
+#define     No              cout << "No" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n, k;
+    cin >> n >> k;
+    vector<int>a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    bool ans = false;
+    int sum = accumulate(all(a), 0);
+    for (int i = 0; i < n; i++) {
+        if(accumulate(a.begin() + i, min(a.end(), a.begin() + i + k), 0) == sum) ans = true; 
+    }
+    if(ans) YES;
+    else NO;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
