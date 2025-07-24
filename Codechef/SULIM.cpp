98bfcd9c1407474f30166436fd504da0d3cdcdd6
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-07-24 08:31:57 +0600

    Create SULIM.cpp

---
+++ Codechef/SULIM.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     Yes             cout << "Yes" << '\n'
+#define     No              cout << "No" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+    int ans = INT64_MIN;
+    for (int L = 0; L <= 100; L++) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += (a[i] > 0 && b[i] <= L) ? a[i] : 0;
+        }
+        ans = max(ans, sum - L);
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
