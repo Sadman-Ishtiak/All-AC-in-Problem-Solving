bf9124060fce26f2519918f41eb3f64026c8329f
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-08-09 11:41:28 +0600

    Create WEIGHTLIFT.cpp

---
+++ Codechef/WEIGHTLIFT.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     Yes             cout << "Yes" << '\n'
+#define     No              cout << "No" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int a, b, c, d, e,f;
+    cin >> a >> b >> c >> d >> e >> f;
+    cout << max(a,b) + max(c,d) + max(e,f) << endl;
+}
+int32_t main() {
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+    return 0;
+}
