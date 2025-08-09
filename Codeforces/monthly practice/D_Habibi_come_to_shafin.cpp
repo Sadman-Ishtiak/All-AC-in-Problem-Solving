ce4b648c00306015ef7611589a22ba10c0c713ab
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-08-09 19:27:46 +0600

    Create D_Habibi_come_to_shafin.cpp

---
+++ Codeforces/monthly practice/D_Habibi_come_to_shafin.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     Yes             cout << "Yes" << '\n'
+#define     No              cout << "No" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n, k;
+    cin >> n >> k;
+    vector<int>a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    map<int, int>prefix_sums;
+    int ans = 0, current_sum = 0;
+    for (int i = 0; i < n; i++) {
+        current_sum += a[i];
+        if(current_sum == k) ans++;
+
+        if(prefix_sums.find(current_sum - k) != prefix_sums.end()) {
+            ans += prefix_sums[current_sum - k];
+        }
+        prefix_sums[current_sum]++;
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
