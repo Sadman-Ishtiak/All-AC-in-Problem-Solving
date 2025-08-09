cd25a1a6088e784b963c156dbbe7b2b1efbb1e7d
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-08-09 19:27:55 +0600

    Create B_Mission_Shiulimala_Kot_Again.cpp

---
+++ Codeforces/monthly practice/B_Mission_Shiulimala_Kot_Again.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     Yes             cout << "Yes" << '\n'
+#define     No              cout << "No" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n, h, r;
+    cin >> n >> h >> r;
+    vector<int>a(n+1, 0);
+    a[0] = 0;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+    // a[n+1] = r;
+    int ans = 0;
+    sort(a)
+    for (int i = 0; i <= n; i++) {
+        if((a[i] - ans) <= h) {
+            ans = a[i];
+            if(ans >= r) {
+                YES;
+                return;
+            }
+        }
+    }
+    if ((r - ans) <= h) YES;
+    else NO;
+}
+int32_t main() {
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+    return 0;
+}
