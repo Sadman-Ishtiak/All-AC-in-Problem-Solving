3216ab56ba6bdbf82f3bef6fba62da7ed551c11f
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-10-25 10:12:01 +0600

    Create 1883C.cpp

---
+++ Codeforces/1883C.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     Yes             cout << "Yes" << '\n'
+#define     No              cout << "No" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n, k, ans = INT64_MAX, even = 0;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        even += (a[i] % 2 == 0);
+        ans = min(ans, (k - (a[i] % k)) % k);
+    }
+    if(k == 2) {
+        if(even > 0) ans = 0;
+        else ans = 1;
+    }
+    if(k == 4) {
+        if(even >= 2) ans = 0;
+        else if(even == 1) ans = min(1LL, ans);
+        else ans = min(2LL, ans);
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
