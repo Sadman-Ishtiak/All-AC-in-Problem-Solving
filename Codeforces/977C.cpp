5b0bc06f3e55ffac2f6d6da28dc284e816f7c67e
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-10-25 10:12:49 +0600

    Create 977C.cpp

---
+++ Codeforces/977C.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     Yes             cout << "Yes" << '\n'
+#define     No              cout << "No" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    int n, k;
+    cin >> n >> k;
+    vector<int>a(n+1, INT64_MAX);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    sort(a)
+    if(a[k] == a[k-1]) {
+        cout << -1 << endl;
+    } else {
+        if(k == 0) {
+            if(a[0] == 1) cout << -1 << endl;
+            else cout << a[0]-1 << endl; 
+        }
+        else cout << a[k-1] << endl;
+    }
+}
+int32_t main() {
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+    return 0;
+}
