47787d5f3dd314c015cd87962e0713935fd28e5e
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-10-25 10:12:50 +0600

    Create A_Why_on_Earth_Did_You_Choose_CSE.cpp

---
+++ Codeforces/Problem Testing/A_Why_on_Earth_Did_You_Choose_CSE.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     Yes             cout << "Yes" << '\n'
+#define     No              cout << "No" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+string reasons[] = {
+"To get a handsome husband who can debug my mood swings.",
+"Because I thought CSE means Cooking, Sewing & Embroidery.",
+"Parents forced me—Ctrl+C my dreams, Ctrl+V into CSE.",
+"To become the next Miss Zuckerberg (but obviously cuter).",
+"Accidentally clicked CSE while filling the form at 2 AM.",
+"To get a cute wife(sondary bou) who understands semicolons.",
+"To hack my ex's Facebook ID (revenge-driven learning).",
+"Because I heard CSE means Collecting Several-Girlfriends Easily.",
+};
+string persoms[] = {
+    "husband",
+    "misunderstand",
+    "force",
+    "zuckerberg",
+    "accident",
+    "wife",
+    "ex",
+    "needgf"
+};
+void solve(){
+    cout << persoms[rand() % 8] << endl;
+}
+int32_t main() {
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+    return 0;
+}
