351c31891ad6d1df6bdd5df7c159e985e5687a3f
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2025-10-25 10:12:53 +0600

    Create C_The_Great_Jump_to_Glory.cpp

---
+++ Codeforces/Problem Testing/C_The_Great_Jump_to_Glory.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define     int             long long
+#define     endl            '\n'
+#define     YES             cout << "YES" << '\n'
+#define     NO              cout << "NO" << '\n'
+#define     Yes             cout << "Yes" << '\n'
+#define     No              cout << "No" << '\n'
+#define     ld              long double
+#define     all(x)          x.begin(),x.end()
+#define     sort(x)         sort(all(x));
+#define     FastIO          ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define     debug(x)        cout << #x << " = " << x << endl;
+#define     fileIO          freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+using namespace std;
+
+void solve(){
+    // cout << rand() << endl;
+    int n, d, ctr = 0;
+    cin >> n >> d;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        if(a[i] > d) ctr++;
+    }
+    cout << ctr << endl;
+}
+int32_t main() {
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+    return 0;
+}
